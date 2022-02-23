b64f728b41e16e4805a374dee7d2a3cdc5ce73ba
jimhome_mac <jsr1611@outlook.com>
2022-02-23 20:45:49 +0900

    initial commit

---
+++ src/main/java/uz/digitalone/houzingapp/HouzingAppApplication.java
@@ -0,0 +1,13 @@
+package uz.digitalone.houzingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouzingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouzingAppApplication.class, args);
+	}
+
+}
