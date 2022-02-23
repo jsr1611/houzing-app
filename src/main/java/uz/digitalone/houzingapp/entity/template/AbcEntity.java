353ddfb3f3c8fd61abce75622702e6781eee8988
Doniyor <komiloff.doniyor2505@gmail.com>
2022-02-24 00:19:01 +0500

    added CRUD for house details

---
+++ src/main/java/uz/digitalone/houzingapp/entity/template/AbcEntity.java
@@ -0,0 +1,53 @@
+package uz.digitalone.houzingapp.entity.template;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class AbcEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @LastModifiedDate
+    private Timestamp updateAt;
+
+    @CreatedBy
+    private Long createdBy;
+
+    @LastModifiedBy
+    private Long updateBy;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        AbcEntity abcEntity = (AbcEntity) o;
+        return id != null && Objects.equals(id, abcEntity.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
