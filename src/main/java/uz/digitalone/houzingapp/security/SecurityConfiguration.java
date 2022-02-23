f81279189f2f9bad64286dd14ffe0d22301d155f
jimhome_mac <jsr1611@outlook.com>
2022-02-24 05:05:52 +0900

    added web security config file

---
+++ src/main/java/uz/digitalone/houzingapp/security/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package uz.digitalone.houzingapp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
