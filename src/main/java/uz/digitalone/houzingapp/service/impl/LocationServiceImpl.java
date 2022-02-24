dff6b373ea3deb3bc01909b145937a8be84ac7df
Muzaffar <muza5660@gmail.com>
2022-02-24 12:01:50 +0500

    Location CRUD edited and tested

--- src/main/java/uz/digitalone/houzingapp/service/impl/LocationServiceImpl.java
+++ src/main/java/uz/digitalone/houzingapp/service/impl/LocationServiceImpl.java
@@ -1,14 +1,15 @@
 package uz.digitalone.houzingapp.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uz.digitalone.houzingapp.dto.LocationDto;
 import uz.digitalone.houzingapp.entity.Location;
 import uz.digitalone.houzingapp.repository.LocationRepository;
+import uz.digitalone.houzingapp.response.Response;
 import uz.digitalone.houzingapp.service.LocationService;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,18 +24,20 @@ public class LocationServiceImpl implements LocationService {
     }
 
     @Override
-    public Location save(LocationDto dto) {
+    public HttpEntity<?> save(LocationDto dto) {
         if (dto.getLongitude() == null || dto.getLatitude() == null)
             throw new RuntimeException("Location not found");
 
-        return locationRepository.save(new Location(dto.getLongitude(), dto.getLatitude()));
+        Location save = locationRepository.save(new Location(dto.getLongitude(), dto.getLatitude()));
+        return ResponseEntity.ok(save);
     }
 
     @Override
-    public Location edit(LocationDto dto, Long id) {
+    public HttpEntity<?> edit(LocationDto dto, Long id) {
         Location location = optionalLocation(id);
         if (location == null){
-            throw new RuntimeException("Location not found");
+            Response response = new Response(false, "Location Not saved", null);
+            return ResponseEntity.ok(response);
         }
         if (!location.getLatitude().equals(dto.getLatitude()))
             location.setLatitude(dto.getLatitude());
@@ -42,31 +45,34 @@ public class LocationServiceImpl implements LocationService {
         if (!location.getLongitude().equals(dto.getLongitude()))
             location.setLongitude(dto.getLongitude());
 
-        return locationRepository.save(location);
+        Location save = locationRepository.save(location);
+        Response response = new Response(true, "Succesffuly save", save);
+
+        return ResponseEntity.ok(response);
     }
 
     @Override
-    public Boolean delete(Long id) {
+    public HttpEntity<?> delete(Long id) {
         Location location = optionalLocation(id);
         if (location == null)
             throw new RuntimeException("Location not found");
 
         locationRepository.delete(location);
-        return true;
+        return ResponseEntity.ok("Successfully delete !!!");
     }
 
     @Override
-    public Location findById(Long id) {
+    public HttpEntity<?> findById(Long id) {
         Location location = optionalLocation(id);
         if (location == null)
             throw new RuntimeException("Location not found");
 
-        return location;
+        return ResponseEntity.ok(location);
     }
 
     @Override
-    public List<Location> findAll() {
-        return locationRepository.findAll();
+    public HttpEntity<?> findAll() {
+        return ResponseEntity.ok(locationRepository.findAll());
     }
 }
 
