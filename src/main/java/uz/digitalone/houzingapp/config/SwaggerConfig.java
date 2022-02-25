66eb719df5d91d72c4045a5c3c29080a6530cce0
Jumanazar <jsr1611@outlook.com>
2022-02-25 11:01:42 +0900

    updated swagger config: added security contexts using singleton collections

--- src/main/java/uz/digitalone/houzingapp/config/SwaggerConfig.java
+++ src/main/java/uz/digitalone/houzingapp/config/SwaggerConfig.java
@@ -2,14 +2,22 @@ package uz.digitalone.houzingapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContextHolder;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -18,10 +26,22 @@ public class SwaggerConfig {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Collections.singletonList(getContext()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("uz.digitalone.houzingapp"))
                 .paths(PathSelectors.any())
                 .build().apiInfo(metadata());
+    }
+
+    private SecurityContext getContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 
     private ApiInfo metadata() {
