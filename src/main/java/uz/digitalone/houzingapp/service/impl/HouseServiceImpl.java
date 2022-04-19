5aba64283741db1b571a0668ae32118fd21dca1d
Jumanazar <jsr1611@outlook.com>
2022-04-19 11:03:09 +0900

    Response status ni HTTP.NOT_FOUND qiladigan qildim. Agar success=false bolsa

--- src/main/java/uz/digitalone/houzingapp/service/impl/HouseServiceImpl.java
+++ src/main/java/uz/digitalone/houzingapp/service/impl/HouseServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
