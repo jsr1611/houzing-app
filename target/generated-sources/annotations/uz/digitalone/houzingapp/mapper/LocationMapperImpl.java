688e1227f11a9ddffd19c0b38eb2057ec2bd96ac
jimhome_mac <jsr1611@outlook.com>
2022-05-20 00:05:11 +0900

    fixed house filter in GET req

---
+++ target/generated-sources/annotations/uz/digitalone/houzingapp/mapper/LocationMapperImpl.java
@@ -0,0 +1,73 @@
+package uz.digitalone.houzingapp.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+import uz.digitalone.houzingapp.dto.request.LocationDto;
+import uz.digitalone.houzingapp.entity.Location;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-05-20T00:00:21+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_302 (Amazon.com Inc.)"
+)
+@Component
+public class LocationMapperImpl implements LocationMapper {
+
+    @Override
+    public LocationDto fromEntity(Location location) {
+        if ( location == null ) {
+            return null;
+        }
+
+        LocationDto locationDto = new LocationDto();
+
+        locationDto.setLongitude( location.getLongitude() );
+        locationDto.setLatitude( location.getLatitude() );
+
+        return locationDto;
+    }
+
+    @Override
+    public List<LocationDto> fromEntities(List<Location> locations) {
+        if ( locations == null ) {
+            return null;
+        }
+
+        List<LocationDto> list = new ArrayList<LocationDto>( locations.size() );
+        for ( Location location : locations ) {
+            list.add( fromEntity( location ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public Location fromDTO(LocationDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Location location = new Location();
+
+        location.setLongitude( dto.getLongitude() );
+        location.setLatitude( dto.getLatitude() );
+
+        return location;
+    }
+
+    @Override
+    public List<Location> fromDTOs(List<LocationDto> dtos) {
+        if ( dtos == null ) {
+            return null;
+        }
+
+        List<Location> list = new ArrayList<Location>( dtos.size() );
+        for ( LocationDto locationDto : dtos ) {
+            list.add( fromDTO( locationDto ) );
+        }
+
+        return list;
+    }
+}
---
+++ target/generated-sources/annotations/uz/digitalone/houzingapp/mapper/UserMapperImpl.java
@@ -0,0 +1,78 @@
+package uz.digitalone.houzingapp.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+import uz.digitalone.houzingapp.dto.request.RegUserDto;
+import uz.digitalone.houzingapp.dto.response.UserDto;
+import uz.digitalone.houzingapp.entity.User;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-05-20T00:00:22+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_302 (Amazon.com Inc.)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserDto fromEntity(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDto userDto = new UserDto();
+
+        userDto.setId( user.getId() );
+        userDto.setFirstname( user.getFirstname() );
+        userDto.setLastname( user.getLastname() );
+        userDto.setEmail( user.getEmail() );
+
+        return userDto;
+    }
+
+    @Override
+    public List<UserDto> fromEntities(List<User> users) {
+        if ( users == null ) {
+            return null;
+        }
+
+        List<UserDto> list = new ArrayList<UserDto>( users.size() );
+        for ( User user : users ) {
+            list.add( fromEntity( user ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public User fromDTO(RegUserDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setFirstname( dto.getFirstname() );
+        user.setLastname( dto.getLastname() );
+        user.setEmail( dto.getEmail() );
+        user.setPassword( dto.getPassword() );
+
+        return user;
+    }
+
+    @Override
+    public List<User> fromDTOs(List<RegUserDto> dtos) {
+        if ( dtos == null ) {
+            return null;
+        }
+
+        List<User> list = new ArrayList<User>( dtos.size() );
+        for ( RegUserDto regUserDto : dtos ) {
+            list.add( fromDTO( regUserDto ) );
+        }
+
+        return list;
+    }
+}
