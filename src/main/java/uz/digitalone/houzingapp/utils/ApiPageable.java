2978e5c336fb979b7aa154e1e61fd27726887226
Jumanazar <jsr1611@outlook.com>
2023-04-25 15:46:25 +0900

    upd error log level to error, upd default paging element number to 10 from 20

--- src/main/java/uz/digitalone/houzingapp/utils/ApiPageable.java
+++ src/main/java/uz/digitalone/houzingapp/utils/ApiPageable.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @ApiImplicitParams({
         @ApiImplicitParam(name = "page", dataType = "integer", paramType = "query", defaultValue = "0", value = "Sahifa indeksi (0..N)"),
-        @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query", defaultValue = "20", value = "Har bitta sahifadagi elementlar soni"),
+        @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query", defaultValue = "10", value = "Har bitta sahifadagi elementlar soni"),
         @ApiImplicitParam(name = "sort", dataType = "string", paramType = "query", value = "Sort qilish criteria si formar: property(,asc|desc)", allowMultiple = true)}
 )
 public @interface ApiPageable {
